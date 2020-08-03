e7d7256f3117596f858dda16d124f8b787389eba
Tanmaya Harichandan <tanmaya0102@gmail.com>
2020-08-04 00:58:04 +0530

    Minor Fix

--- app/src/main/java/com/tanmaya/yaayde/StudentActivity.java
+++ app/src/main/java/com/tanmaya/yaayde/StudentActivity.java
@@ -8,18 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class StudentActivity extends AppCompatActivity {
-Button adds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
-        adds=findViewById(R.id.adds);
-        adds.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(StudentActivity.this,AddStudent.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
--- app/src/main/java/com/tanmaya/yaayde/TeacherActivity.java
+++ app/src/main/java/com/tanmaya/yaayde/TeacherActivity.java
@@ -2,13 +2,25 @@ package com.tanmaya.yaayde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TeacherActivity extends AppCompatActivity {
-
+    Button adds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+        adds=findViewById(R.id.adds);
+        adds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(TeacherActivity.this,AddStudent.class);
+                startActivity(intent);
+            }
+        });
     }
 }
